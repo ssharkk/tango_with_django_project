9a13aa3e841d8f5bf9c32ca6531ebd8e3e3a5622
Adrian Durci <2403010d@student.gla.ac.uk>
2020-01-25 19:35:47 +0000

    Fixed spelling to match chapter3 tests

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Rango says hey there partner!<br>" 
-						+ "<a href='/rango/about'>About me</a>")
+						+ "<a href='/rango/about/'>About</a>")
 
 def about(request):
 	return HttpResponse("Rango says here is the about page.<br>" 
-						+ "<a href='/rango'>Go back</a>")
+						+ "<a href='/rango/'>Index</a>")
