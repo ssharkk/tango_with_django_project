b73e0f13dc2a422c4a8a093dc8341ba4e64b40c0
Adrian Durci <2403010d@student.gla.ac.uk>
2020-02-01 19:55:25 +0000

    (Chapter 5) Added Rango database

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+class TabPages(admin.TabularInline):
+    model = Page
+    fields = ["title", "url", "views"]
+    extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+#    fields = ["likes", "name", "views"]
+    inlines = [TabPages]
+    list_display = ("name", "likes", "views")
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url")
+
+# Register the models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
