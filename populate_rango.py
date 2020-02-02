e94fae094717f4415c3ce07602dca4689367eed9
Adrian Durci <2403010d@student.gla.ac.uk>
2020-02-02 01:40:49 +0000

    (Chapter 6) Created data driven pages, showing categories and pages from database.

--- populate_rango.py
+++ populate_rango.py
@@ -11,18 +11,18 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-        {'title': 'Official Python Tutorial', 'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'How to Think like a Computer Scientist', 'url': 'http://www.greenteapress.com/thinkpython/'},
-        {'title': 'Learn Python in 10 Minutes', 'url': 'http://www.korokithakis.net/tutorials/python/'},
+        {'title': 'Official Python Tutorial', 'url': 'http://docs.python.org/3/tutorial/', 'views': 15},
+        {'title': 'How to Think like a Computer Scientist', 'url': 'http://www.greenteapress.com/thinkpython/', 'views': 51},
+        {'title': 'Learn Python in 10 Minutes', 'url': 'http://www.korokithakis.net/tutorials/python/', 'views': 113},
     ]
     django_pages = [
-        {'title': 'Official Django Tutorial', 'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/'},
-        {'title': 'How to Tango with Django', 'url': 'http://www.tangowithdjango.com/'},
+        {'title': 'Official Django Tutorial', 'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 73},
+        {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/', 'views': 11},
+        {'title': 'How to Tango with Django', 'url': 'http://www.tangowithdjango.com/', 'views': 2},
     ]
     other_pages = [
-        {'title': 'Bottle', 'url': 'http://bottlepy.org/docs/dev/'},
-        {'title': 'Flask', 'url': 'http://flask.pocoo.org'},
+        {'title': 'Bottle', 'url': 'http://bottlepy.org/docs/dev/', 'views': 78},
+        {'title': 'Flask', 'url': 'http://flask.pocoo.org', 'views': 231},
     ]
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
@@ -31,7 +31,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- rango/admin.py
+++ rango/admin.py
@@ -9,11 +9,12 @@ class TabPages(admin.TabularInline):
 
 class CategoryAdmin(admin.ModelAdmin):
 #    fields = ["likes", "name", "views"]
+    prepopulated_fields = {"slug": ('name',)}
     inlines = [TabPages]
     list_display = ("name", "likes", "views")
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ("title", "category", "url")
+    list_display = ("title", "category", "url", "views")
 
 # Register the models here.
 admin.site.register(Category, CategoryAdmin)
