0f33b82dc5fa44b37702789a3fc594263261857d
Adrian Durci <2403010d@student.gla.ac.uk>
2020-02-09 01:09:48 +0000

    (Chapter 7) Added forms to add new categories and pages.

---
+++ rango/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from rango.models import Page, Category
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
+                           help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Category
+        fields = ('name',)
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH,
+                            help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=Page.URL_MAX_LENGTH,
+                         help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        print("ive got this url: ", url)
+
+        if url and not (url.startswith('http://') or url.startswith('https://')):
+            url = f'http://{url}'
+            print("changed it and now got this url: ", url)
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
